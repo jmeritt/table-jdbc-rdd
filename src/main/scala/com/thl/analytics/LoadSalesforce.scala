eeafc735e0d9aae7eb4500fa8e6b260927974033
jmeritt <jaime.meritt@gmail.com>
2015-02-17 14:57:04 -0500

    Integrating into IdeaProject

--- src/main/scala/com/thl/analytics/LoadSalesforce.scala
+++ src/main/scala/com/thl/analytics/LoadSalesforce.scala
@@ -1,14 +1,11 @@
-/**
- * Created by jmeritt on 2/8/15.
- */
 
 package com.thl.analytics
 
 import java.sql.DriverManager
 
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql._
 import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 object LoadSalesforce {
 
@@ -17,7 +14,6 @@ object LoadSalesforce {
   val url = "jdbc:datadirect:ddcloud://service.datadirectcloud.com;databaseName=SFDC"
   val username = "jmeritt"
   val password = "7ju$u7kJ"
-  val sparkHome = "/Users/jmeritt/Documents/Development/spark-1.2.0"
 
   val jars = Array(
     "/Users/jmeritt/Documents/Development/TableJdbcRDD/target/scala-2.10/table-jdbcrdd_2.10-0.1.0.jar",
@@ -27,7 +23,7 @@ object LoadSalesforce {
 
   def main(args: Array[String]) {
     Class.forName(driverClass)
-    val conf = new SparkConf().setAppName("SFDCAnalytics").setMaster("local").setJars(jars);
+    val conf = new SparkConf().setAppName("SFDCAnalytics").setMaster("local").setJars(jars)
     val sc = new SparkContext(conf)
     val rdd = new TableJdbcRDD[Row](sc, () => DriverManager.getConnection(url, username, password), "Account", 8)
     rdd.persist()
